15659fe8c3284ea64ae89d7024846f9454800555
Sheena Nickerson <sheenanick@gmail.com>
2017-03-12 23:17:59 -0700

    Add auth state listener to app component to show log out button if user is logged in

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './common';
+import { Header, Spinner, Button, CardSection } from './common';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBtrkq_ZwbgsT-EH43ahD6IKTEeuXHIO1o',
@@ -14,14 +16,41 @@ class App extends Component {
       storageBucket: 'react-native-auth-397c0.appspot.com',
       messagingSenderId: '64582402186'
     });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button text='Log Out' callback={() => firebase.auth().signOut()} />
+          </CardSection>
+
+        );
+      case false:
+        return (
+          <View>
+            <LoginForm />
+            <SignUpForm />
+          </View>
+        );
+      default:
+        return <Spinner />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
-        <SignUpForm />
+        {this.renderContent()}
       </View>
     );
   }
